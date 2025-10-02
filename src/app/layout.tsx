bba828c30af47dcd22cca16bdbc8b516c94065f9
Mohit Jain <mohitjain7730@gmail.com>
2025-10-02 07:31:49 +0000

    I am trying to turn on google adsense for my site, it is showing this ms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5405909046385135"
+     crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
